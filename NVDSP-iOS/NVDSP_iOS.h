deece52b020c813f5310fd69244da963204e6672
URABE Shumpei <urabe@meteor.ne.jp>
2019-07-24 16:27:41 +0900

    Initial Commit

---
+++ NVDSP-iOS/NVDSP_iOS.h
@@ -0,0 +1,19 @@
+//
+//  NVDSP_iOS.h
+//  NVDSP-iOS
+//
+//  Created by meteor on 2019/07/24.
+//  Copyright © 2019 meteor Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NVDSP_iOS.
+FOUNDATION_EXPORT double NVDSP_iOSVersionNumber;
+
+//! Project version string for NVDSP_iOS.
+FOUNDATION_EXPORT const unsigned char NVDSP_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVDSP_iOS/PublicHeader.h>
+
+
