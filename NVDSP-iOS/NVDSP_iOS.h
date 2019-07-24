f38c7f959c4ef3f6b80d2ca036c96dab80a94862
URABE Shumpei <urabe@meteor.ne.jp>
2019-07-25 02:35:50 +0900

    Add headers

--- NVDSP-iOS/NVDSP_iOS.h
+++ NVDSP-iOS/NVDSP_iOS.h
@@ -17,3 +17,14 @@ FOUNDATION_EXPORT const unsigned char NVDSP_iOSVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <NVDSP_iOS/PublicHeader.h>
 
 #import "NVDSP.h"
+#import "Filters/NVBandpassQPeakGainFilter.h"
+#import "Filters/NVHighShelvingFilter.h"
+#import "Filters/NVAllpassFilter.h"
+#import "Filters/NVLowpassFilter.h"
+#import "Filters/NVBandpassFilter.h"
+#import "Filters/NVHighpassFilter.h"
+#import "Filters/NVLowShelvingFilter.h"
+#import "Filters/NVNotchFilter.h"
+#import "Filters/NVPeakingEQFilter.h"
+#import "Utilities/NVClippingDetection.h"
+#import "Utilities/NVSoundLevelMeter.h"
